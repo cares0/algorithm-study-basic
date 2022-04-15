5075d6e4b4b543a36a4f18df7e4b584b652432e9
cares0 <cares00000@gmail.com>
2022-04-15 17:52:23 +0900

    Part2-p12
    남은 일 수 구하기, 진행된 일 수 구하기

--- src/part2/dataStructureBasic/c1/Practice12.java
+++ src/part2/dataStructureBasic/c1/Practice12.java
@@ -15,13 +15,25 @@ public class Practice12 {
 		return (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) ? 1 : 0;
 	}
 
-	// y년 m월 d일의 그 해 경과일수를 구함
+	// Q8. 메서드 dayOfYear를 변수 i와 days 없이 구현하시오. while문을 사용하시오
 	static int dayOfYear(int y, int m, int d) {
-		int days = d;
 		
-		for(int i=1; i<m; i++) {
-			days += mdays[isLeap(y)][i-1];
+		while(m > 1) {
+			d += mdays[isLeap(y)][--m];
 		}
+		
+		return d;
+	}
+	
+	// Q9. y년 m월 d일의 그 해 남은 일 수를 구하는 메서드를 작성하시오
+	static int leftDayOfYear(int y, int m, int d) {
+		int isLeap = isLeap(y);
+		int days = mdays[isLeap][m-1] - d + 1;
+		
+		for(int i=0; i<12-m;i++) {
+			days += mdays[isLeap][12-i-1];
+		}
+		
 		return days;
 	}
 		
@@ -41,6 +53,7 @@ public class Practice12 {
 			int day = sc.nextInt();
 			
 			System.out.printf("그 해 %d일째 입니다.\n", dayOfYear(year, month, day));
+			System.out.printf("남은 기간은 %d일 입니다. \n", leftDayOfYear(year, month, day));
 			
 			System.out.print("한 번 더 할까요? (1.예 / 0.아니오) : ");
 			retry = sc.nextInt();			
