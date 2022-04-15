cb06210d1790a966e105aa1055768a457c1a919c
cares0 <cares00000@gmail.com>
2022-04-15 15:05:08 +0900

    Part2-p11-3
    3. 1000이하의 정수 중 소수 구하기 최종

--- src/part2/dataStructureBasic/c1/Practice11.java
+++ src/part2/dataStructureBasic/c1/Practice11.java
@@ -8,19 +8,22 @@ public class Practice11 {
 		int[] prime = new int[500];
 		
 		prime[ptr++] = 2;
+		prime[ptr++] = 3;
 		
-		for(int n = 3; n <= 1000; n+= 2) { // 대상은 홀수만(짝수는 모두 2로 나눠지기 때문)
-			int i;
-			for(i = 1; i < ptr; i++) { // 이미 찾은 소수로만 나누면 됨 ptr(소수 배열의 크기)만 반복
-				counter++;
+		for(int n = 5; n <= 1000; n+= 2) { // 대상은 홀수만(짝수는 모두 2로 나눠지기 때문)
+			boolean flag = false;
+			for(int i = 1; prime[i] * prime[i] <= n; i++) { 
+				// 이미 찾은 소수로만 나누면 됨 ptr(소수 배열의 크기)만 반복
+				// 단, 이미 찾은 소수의 제곱보다 n이 작다면 나눠보지 않아도 됨
+				counter += 2;
 				if(n % prime[i] == 0) { // 나눠진다면 더 이상 진행X
+					flag = true; // 이 경우는 소수가 아닌 경우이기에
 					break;
 				}
 			}
-			if(ptr == i) { // 배열의 마지막인덱스 숫자까지 나눠지지 않았다면 소수임
+			if(!flag) { // 소수인 경우에 저장
 				prime[ptr++] = n;
 			}
-			
 				
 		}
 		
