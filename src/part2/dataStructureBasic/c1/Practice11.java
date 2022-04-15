e7d971ac6d4af4a5ab0510199133c854b4e44cb4
cares0 <cares00000@gmail.com>
2022-04-15 14:15:03 +0900

    Part2-p11-2
    2. 1000이하의 정수 중 소수 구하기 두 번째 알고리즘

--- src/part2/dataStructureBasic/c1/Practice11.java
+++ src/part2/dataStructureBasic/c1/Practice11.java
@@ -4,21 +4,31 @@ public class Practice11 {
 
 	public static void main(String[] args) {
 		int counter = 0; // 나눗셈의 횟수
+		int ptr = 0; // 찾은 소수의 갯수
+		int[] prime = new int[500];
 		
-		for(int n = 2; n<= 1000; n++) {
+		prime[ptr++] = 2;
+		
+		for(int n = 3; n <= 1000; n+= 2) { // 대상은 홀수만(짝수는 모두 2로 나눠지기 때문)
 			int i;
-			for(i = 2; i < n; i++) {
+			for(i = 1; i < ptr; i++) { // 이미 찾은 소수로만 나누면 됨 ptr(소수 배열의 크기)만 반복
 				counter++;
-				if(n % i == 0) {
-					break; // 더 이상 소수가 아님
+				if(n % prime[i] == 0) { // 나눠진다면 더 이상 진행X
+					break;
 				}
 			}
-			if(n == i) { // 나머지가 0인 경우가 없다는 소리임
-				System.out.println(n); // 소수
+			if(ptr == i) { // 배열의 마지막인덱스 숫자까지 나눠지지 않았다면 소수임
+				prime[ptr++] = n;
 			}
 			
+				
 		}
-		System.out.println("나눗셈을 수행한 횟수 " + counter);
+		
+		for(int i=0;i<ptr;i++) {
+			System.out.println(prime[i]);
+		}
+		
+		System.out.println("횟수 : " + counter);
 
 	}
 
