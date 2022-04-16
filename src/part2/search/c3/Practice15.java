5e0e89506d5ced6e0bbc2e6240dc9957b2365b2b
cares0 <cares00000@gmail.com>
2022-04-16 21:45:53 +0900

    Part3-p15-ex
    이진검색

---
+++ src/part2/search/c3/Practice15.java
@@ -0,0 +1,56 @@
+package part2.search.c3;
+
+import java.util.Scanner;
+
+public class Practice15 {
+	
+	static int binSearch(int[] a, int n, int key) {
+		int start = 0;
+		int end = n - 1;
+		
+		do {
+			int center = (start + end) / 2;
+			if(a[center] == key) {
+				return center;
+			} else if(a[center] < key) {
+				start = center + 1;
+			} else {
+				end = center - 1;
+			}
+		} while(start <= end);
+		return - 1;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("요솟수 : ");
+		int num = sc.nextInt();
+		int[] x = new int[num];
+		
+		System.out.println("오름차순으로 입력하세요.");
+		
+		System.out.print("x[0] : ");
+		x[0] = sc.nextInt();
+		
+		for(int i=1;i<num;i++) {
+			do {
+				System.out.print("x[" + i + "] : ");
+				x[i] = sc.nextInt();
+			} while(x[i-1] > x[i]);
+		}
+		
+		System.out.print("검색할 값 : ");
+		int key = sc.nextInt();
+		int idx = binSearch(x, num, key);
+		
+		if(idx == -1) {
+			System.out.println("찾는 값이 없습니다.");
+		} else {
+			System.out.println(idx + "번째 인덱스에 있습니다.");
+		}
+		
+
+	}
+
+}
