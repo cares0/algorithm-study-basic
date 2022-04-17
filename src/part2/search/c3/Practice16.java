d40500abe896d009011c5391de7cd6f271ce812a
cares0 <cares00000@gmail.com>
2022-04-17 12:21:53 +0900

    Part3-p16
    내림차순 정렬

--- src/part2/search/c3/Practice16.java
+++ src/part2/search/c3/Practice16.java
@@ -38,25 +38,42 @@ public class Practice16 {
 		}
 
 		public static final Comparator<PhyscData> HEIGHT_ORDER = new HeightOrderComparator();
+		
+		// Q7. 시력에 대한 내림차순 정렬의 신체검사 데이터에서 특정 시력을 가진 사람을 검색하는 프로그램을 작성하시오.
+		private static class VisionOrderComparator implements Comparator<PhyscData> {
+
+			@Override
+			public int compare(PhyscData o1, PhyscData o2) {
+				if(o1.vision < o2.vision) {
+					return 1;
+				} else if(o1.vision > o2.vision) {
+					return -1;
+				} else {
+					return 0;					
+				}
+			}	
+		}
+		
+		public static final Comparator<PhyscData> VISION_ORDER = new VisionOrderComparator();
 	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
 		PhyscData[] x = {
-				new PhyscData("강민하", 162, 0.3),
-				new PhyscData("이수연", 168, 0.4),
-				new PhyscData("황지안", 169, 0.8),
-				new PhyscData("유서범", 171, 1.5),
-				new PhyscData("김찬우", 173, 0.7),
-				new PhyscData("장경오", 174, 1.2),
-				new PhyscData("박준서", 175, 2.0),
+				new PhyscData("강민하", 162, 1.3),
+				new PhyscData("이수연", 168, 1.2),
+				new PhyscData("황지안", 169, 0.9),
+				new PhyscData("유서범", 171, 0.7),
+				new PhyscData("김찬우", 173, 0.6),
+				new PhyscData("장경오", 174, 0.5),
+				new PhyscData("박준서", 175, 0.3),
 		};
 
-		System.out.print("몇 cm를 찾을까요?");
+		System.out.print("시력 입력");
 
-		int height = sc.nextInt();
-		int idx = Arrays.binarySearch(x, new PhyscData("", height, 0.0) , PhyscData.HEIGHT_ORDER);
+		double vision = sc.nextDouble();
+		int idx = Arrays.binarySearch(x, new PhyscData("", 0, vision) , PhyscData.VISION_ORDER);
 		
 		if(idx < 0) {
 			System.out.println("찾는 사람이 없습니다.");
