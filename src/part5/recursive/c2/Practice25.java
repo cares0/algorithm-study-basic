3fd3a1919c934075155bef778f7afc3d7491c382
cares0 <cares00000@gmail.com>
2022-04-23 17:23:52 +0900

    Part5-p25-ex
    비재귀적 표현방법

--- src/part5/recursive/c2/Practice25.java
+++ src/part5/recursive/c2/Practice25.java
@@ -13,12 +13,22 @@ public class Practice25 {
 		}
 	}
 	
+	static void recur2(int n) {
+		while(n > 0) {
+			int temp = n;
+			n = n - 1;
+			System.out.println(temp);
+			n = temp - 2;
+		}
+	}
+	
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("정수 입력 : ");
 		int x = sc.nextInt();
 		
-		recur(x);
+		recur2(x);
 	}
 }
